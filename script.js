2176c5688bf87ea34a136eae8fde7aa76367f65f
unknown <josevictormobile@gmail.com>
2021-08-06 23:43:50 -0300

    Consulta CEP

---
+++ script.js
@@ -0,0 +1,28 @@
+function consultaCEP(){
+    var cep = document.getElementById('cep').value.replace(/\D/g,'');
+    console.log(cep);
+    var url = "https://viacep.com.br/ws/"+cep+"/json/";
+    console.log(url);
+    var request = new XMLHttpRequest();
+
+    request.open('GET', url);
+
+    request.onload = ()=>{
+        var response = JSON.parse(request.responseText);
+
+        if(response.erro === true){
+            document.getElementById('return').innerHTML = 'CEP NÃO ENCONTRADO';
+        }else{
+            console.log(response);
+            document.getElementById('return').innerHTML = 
+            
+            'CEP: '+ response.cep+'<br>'+
+            'Logradouro: '+ response.logradouro+'<br>'+
+            'CEP: '+ response.bairro+'<br>'+
+            'Cidade/UF: '+ response.localidade+' / '+ response.uf;
+        }
+    }
+    
+    request.send();
+
+}
